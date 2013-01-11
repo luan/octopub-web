229f7902f77f2a87e5bba280c3fecd78e8d048b0
Luan Santos <cfcluan@gmail.com>
2013-01-11 13:16:53 -0800

    Use different token when available on ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Octopub::Application.config.secret_token = '12b032a4d727ccfc8c6ef7bb9fc4cd23e703affd67ce53b56c3407516d42b5b8bba04a512e429aa7a0a18c094ee2fa9fae8930da42ad2dd116aa5b0a2678bbcf'
+Octopub::Application.config.secret_token = ENV['SECRET_TOKEN'] || '12b032a4d727ccfc8c6ef7bb9fc4cd23e703affd67ce53b56c3407516d42b5b8bba04a512e429aa7a0a18c094ee2fa9fae8930da42ad2dd116aa5b0a2678bbcf'
